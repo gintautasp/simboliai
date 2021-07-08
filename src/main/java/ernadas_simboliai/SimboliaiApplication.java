8f01f01ae5e7cfaf9bed0de14c90651272313e30
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2021-07-08 12:31:28 +0300

    pradinis karkasas

---
+++ src/main/java/ernadas_simboliai/SimboliaiApplication.java
@@ -0,0 +1,13 @@
+package ernadas_simboliai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimboliaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimboliaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ernadas_simboliai/SimboliaiApplicationTests.java
@@ -0,0 +1,13 @@
+package ernadas_simboliai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimboliaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
